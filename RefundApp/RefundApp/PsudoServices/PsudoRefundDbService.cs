3003cd4ad61fc48dfcbdf5b781aea3131da9438d
40gilad <40gilad.shenkar@gmail.com>
2024-10-13 18:54:00 +0300

    changed names to psudoDbservices and added usersDbService

---
+++ RefundApp/RefundApp/PsudoServices/PsudoRefundDbService.cs
@@ -0,0 +1,56 @@
+﻿using RefundApp.Models;
+using System.ComponentModel;
+
+namespace RefundApp.PsudoServices
+{
+    public class PsudoRefundDbService
+    {
+        private static Dictionary<string, RefundModel> refunds;
+        private static PsudoRefundDbService instance;
+        private static readonly object lockObj = new object();
+
+        private PsudoRefundDbService()
+        {
+            refunds = new Dictionary<string, RefundModel>();
+        }
+
+        public static PsudoRefundDbService Instance()
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                    instance = new PsudoRefundDbService();
+                return instance;
+            }
+        }
+
+        public Dictionary<string, RefundModel> Get()
+        {
+            refunds = new Dictionary<string, RefundModel>();
+            return refunds;
+        }
+
+        public RefundModel Get(string order_id)
+        {
+            if (refunds==null || !refunds.ContainsKey(order_id))
+                throw new KeyNotFoundException($"No refund found with OrderId: {order_id}");
+            return refunds[order_id];
+        }
+
+        public void Add(RefundModel refund)
+        {
+            refunds = new Dictionary<string, RefundModel>();
+
+            if (refund != null)
+                refunds.Add(refund.OrderId, refund);
+
+        }
+
+        public void Remove(string order_id)
+        {
+            if (!refunds.ContainsKey(order_id))
+                throw new KeyNotFoundException($"No refund found with OrderId: {order_id}");
+            refunds.Remove(order_id);
+        }
+    }
+}
---
+++ RefundApp/RefundApp/PsudoServices/PsudoUserDbService.cs
@@ -0,0 +1,65 @@
+﻿using RefundApp.Models;
+using RefundApp.Utils;
+using System.Collections.Generic;
+
+namespace RefundApp.PsudoServices
+{
+    public class PsudoUserDbService
+    {
+        private static Dictionary<string, UserModel> users;
+        private static PsudoUserDbService instance;
+        private static readonly object lockObj = new object();
+
+        private PsudoUserDbService()
+        {
+            users = new Dictionary<string, UserModel>();
+
+        }
+        public static PsudoUserDbService Instance()
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                    instance = new PsudoUserDbService();
+                return instance;
+            }
+        }
+
+        public Dictionary<string, UserModel> Get()
+        {
+            return users;
+        }
+
+        public UserModel Get(string user_id)
+        {
+            if (!users.ContainsKey(user_id))
+                throw new KeyNotFoundException($"No refund found with OrderId: {user_id}");
+            return users[user_id];
+        }
+
+        public void Add(UserModel user)
+        {
+            if (user != null)
+            {
+                if (users.ContainsKey(user.Uid))
+                    throw new InvalidOperationException($"User ID {user.Uid} already exists.");
+
+                users.Add(user.Uid, user);
+            }
+        }
+
+        public void Update(UserModel u)
+        {
+            if (u == null)
+                throw new NullReferenceException();
+            else users[u.Uid].UName = u.UName;
+        }
+
+        public void Remove(string user_id)
+        {
+            if (!users.ContainsKey(user_id))
+                throw new KeyNotFoundException($"No user found with UserId: {user_id}");
+            users.Remove(user_id);
+        }
+    }
+}
