11c0865cbc26d8446fc8213493d65ca8ed486a1d
40gilad <40gilad.shenkar@gmail.com>
2024-10-20 12:30:44 +0300

    added session id and contructor from dict<string,string>

--- RefundApp/RefundApp/Models/UserModel.cs
+++ RefundApp/RefundApp/Models/UserModel.cs
@@ -1,14 +1,17 @@
-﻿namespace RefundApp.Models
+﻿using System.Xml.Linq;
+
+namespace RefundApp.Models
 {
     public class UserModel
     {
         public string UName { get; set; }
         public string UEmail { get; set; }
         public string UPassword { get; set; }
+        public string SessionId { get; set; }
 
         public UserModel() { }
 
-        public UserModel(string u_name, string e_mail, string u_password)
+        public UserModel(string u_name, string e_mail, string u_password,string session_id="")
         {
             if (string.IsNullOrWhiteSpace(u_password))
                 throw new ArgumentException("Password cannot be null or empty", nameof(u_password));
@@ -20,6 +23,7 @@
             UName = u_name;
             UEmail = e_mail;
             UPassword = u_password;
+            SessionId = session_id;
         }
 
         public UserModel(UserModel other)
@@ -31,6 +35,24 @@
             UEmail = other.UEmail;
             UPassword = other.UPassword;
         }
+
+        public UserModel(Dictionary<string, string> data)
+        {
+            if (string.IsNullOrWhiteSpace(data["userName"]))
+                throw new ArgumentException("Password cannot be null or empty", nameof(data));
+            if (string.IsNullOrWhiteSpace(data["email"]))
+                throw new ArgumentException("Email cannot be null or empty", nameof(data));
+            if (string.IsNullOrWhiteSpace(data["password"]))
+                throw new ArgumentException("User Name cannot be null or empty", nameof(data));
+
+            UName = data["userName"];
+            UEmail = data["email"];
+            UPassword = data["password"];
+            if (data.ContainsKey("session"))
+                SessionId = data["session"];
+        }
+
+        
 
         public override string ToString()
         {
