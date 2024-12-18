176adf7b96da327fb137c4b4ba39d8802345f779
40gilad <40gilad.shenkar@gmail.com>
2024-12-18 11:59:13 +0200

    added customer name handling hebrew and english

--- RefundApp/CsvProcessor/playground.py
+++ RefundApp/CsvProcessor/playground.py
@@ -64,6 +64,47 @@ def listify_each_record(text):
     return listed
 
 
+def normilize_name(record, customer_index, is_hebrew):
+    if is_hebrew:
+        customer_name = f'{record[customer_index - 1]} {record[customer_index - 2]}'
+        customer_name = customer_name[::-1]
+        first_name = customer_name.split(" ")[1].replace(".", "")
+        last_name = customer_name.split(" ")[0].replace(".", "")
+    else:
+        customer_name = f'{record[customer_index + 1]} {record[customer_index + 2]}'
+        first_name = customer_name.split(" ")[0].replace(".", "")
+        last_name = customer_name.split(" ")[1].replace(".", "")
+    return f'{first_name} {last_name}'
+
+
+def get_customer_index(record):
+    is_hebrew = False
+    try:
+        customer_index = record.index("Customer:")
+        # english
+    except ValueError:
+        try:
+            customer_index = record.index(":Customer")
+            is_hebrew = True
+            # hebrew
+        except ValueError:
+            try:
+                customer_index = record.index(":Customer:")
+                # not sure
+                kaki = 1
+            except ValueError:
+                return None
+    return {'customer_index': customer_index, 'is_hebrew': is_hebrew}
+
+
+def get_customer_name(record):
+    try:
+        name = normilize_name(record, **get_customer_index(record))
+    except ValueError:
+        return ""
+    return name
+
+
 def dictify_relevant_data_from_record(text):
     ret_list = list()
     listed = listify_each_record(text)
@@ -72,16 +113,9 @@ def dictify_relevant_data_from_record(text):
         amount = curr_line[1]
         if amount[0] == "-":
             amount = amount[1:]
-        ret_list.append({"order_id": curr_line[4], "amount": amount})
+        customer_name = get_customer_name(curr_line)
+        ret_list.append({"order_id": curr_line[4],"customer":customer_name, "amount": amount})
     return ret_list
-
-
-def dictify_wolt_to_rest(text):
-    extract_text_till_sentence(text, )
-
-
-def dictify_rest_to_wolt(text):
-    pass
 
 
 def extract_json_pdf(file_path):
